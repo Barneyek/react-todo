5573108c8514dad8ce213e2f48945eb4868c626b
Damian Osiecki <osieckidamian1992@gmail.com>
2020-05-06 17:27:05 +0200

    Fix styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div id="app" className="container">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+        <div className="todo-container">
+          <input type="text" className="todo-input" placeholder="What needs to be done" />
+        </div>
+        <div class="extra-container">
+          <label>
+            <input type="checkbox" />Check All
+          </label>
+          <div>2 items left</div>
+        </div>
+        <div class="extra-container">
+          <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+          <div>
+            <button>Clear completed</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
